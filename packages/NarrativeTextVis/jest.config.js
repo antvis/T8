251e9ced91d50a787a781d87621f0046bd90bffc
lixi <1491812683@qq.com>
2021-08-06 14:50:28 +0800

    chore: 🔧 add jest

---
+++ packages/NarrativeTextVis/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.ts'],
+  testRegex: '(/__tests__/.*\\.(test|spec))\\.ts$',
+  verbose: false,
+};
