90c6feae45bdc19a511f4eb3afb647bc2ad6bb1d
lixi <1491812683@qq.com>
2021-08-11 12:55:37 +0800

    feat(ntv): ✨ export Phrase components

--- packages/NarrativeTextVis/src/phrases/index.tsx
+++ packages/NarrativeTextVis/src/phrases/index.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import { map } from 'lodash';
 import { IPhrase } from '@antv/text-schema';
 import { DetailChartDisplayType } from '../interface';
-
-import { Default } from './Default';
-import { DeltaValue } from './DeltaValue';
-import { RatioValue } from './RatioValue';
-import { DimValue } from './DimValue';
-import { TrendDesc } from './TrendDesc';
+import { Phrase } from './Phrase';
 
 interface Props {
   spec: IPhrase[];
@@ -17,24 +12,24 @@ interface Props {
 export const Phrases: React.FC<Props> = ({ spec, detailChartDisplayType }) => (
   <>
     {map(spec, (phrase, index) => {
-      const defaultComp = <Default phrase={phrase} key={`${index}-${phrase.value}`} />;
+      const defaultComp = <Phrase phrase={phrase} key={`${index}-${phrase.value}`} />;
       if (phrase.type === 'text') return defaultComp;
       if (phrase?.metadata?.entityType) {
         switch (phrase?.metadata?.entityType) {
           case 'trend_desc':
             return (
-              <TrendDesc
+              <Phrase.TrendDesc
                 phrase={phrase}
                 detailChartDisplayType={detailChartDisplayType}
                 key={`${index}-${phrase.value}`}
               />
             );
           case 'delta_value':
-            return <DeltaValue phrase={phrase} key={`${index}-${phrase.value}`} />;
+            return <Phrase.DeltaValue phrase={phrase} key={`${index}-${phrase.value}`} />;
           case 'ratio_value':
-            return <RatioValue phrase={phrase} key={`${index}-${phrase.value}`} />;
+            return <Phrase.RatioValue phrase={phrase} key={`${index}-${phrase.value}`} />;
           case 'dim_value':
-            return <DimValue phrase={phrase} key={`${index}-${phrase.value}`} />;
+            return <Phrase.DimValue phrase={phrase} key={`${index}-${phrase.value}`} />;
           default:
             return defaultComp;
         }
@@ -43,3 +38,5 @@ export const Phrases: React.FC<Props> = ({ spec, detailChartDisplayType }) => (
     })}
   </>
 );
+
+export { Phrase } from './Phrase';
