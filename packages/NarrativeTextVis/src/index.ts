b202e49fbc9391bb3d992f35c041cf3b9b4eb7ca
lixi <1491812683@qq.com>
2021-10-24 22:39:42 +0800

    feat(ntv): ✨ export prefix icon

--- packages/NarrativeTextVis/src/index.ts
+++ packages/NarrativeTextVis/src/index.ts
@@ -4,5 +4,6 @@ export { NarrativeTextVis, NarrativeTextVisProps } from './NarrativeTextVis';
 export { Section } from './section';
 export { Paragraph, Headline } from './paragraph';
 export { Phrase } from './phrases';
+export { ArrowUp, ArrowDown } from './assets/icons';
 export * from './tools';
 export * from '@antv/text-schema';
