e27972046fa3842f57578c9939368e0ce7594380
lixi <1491812683@qq.com>
2022-02-08 11:45:10 +0800

    chore(wsc): 🔧 init word-scale chart pkg

---
+++ packages/word-scale-chart/rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: './dist/index.min.js',
+    format: 'umd',
+    sourcemap: false,
+    name: 'word-scale-chart',
+  },
+  plugins: [resolve(), commonjs(), typescript(), terser()],
+};
