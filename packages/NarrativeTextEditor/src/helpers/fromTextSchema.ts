8ea134d8842e37fb139df6f7e22a177105ab6a2a
chenluli <chenluuli@gmail.com>
2022-06-13 16:33:14 +0800

    feat: ✨ 增加对 sub bullets 的处理

---
+++ packages/NarrativeTextEditor/src/helpers/fromTextSchema.ts
@@ -0,0 +1,132 @@
+import type {
+  BulletsParagraphSpec,
+  HeadingParagraphSpec,
+  NarrativeTextSpec,
+  ParagraphSpec,
+  PhraseSpec,
+  TextParagraphSpec,
+} from '@antv/narrative-text-vis';
+import type { TDescendant } from '@udecode/plate';
+import type { NarrativeTextEditorProps } from '../types';
+
+// TODO #羽然 对于已知类型，PhraseSpec， HeadlineSpec， ParagraphSpec 可以转换；转换到 custom Spec 还有问题
+
+// 将 text-schema 转换成 NarrativeVisEditor 需要的格式
+export const specToEditorProps = (
+  spec: NarrativeTextSpec,
+): {
+  content?: NarrativeTextEditorProps['initialValue'];
+  variableMap: NarrativeTextEditorProps['variableMap'];
+} => {
+  const variableMap = {};
+  const content: TDescendant[] = [];
+
+  let variableIndex = -1;
+  // phrase 元素转换
+  const convertPhrase = (phrase: PhraseSpec) => {
+    switch (phrase.type) {
+      case 'text':
+        return {
+          text: phrase.value,
+        };
+      case 'entity': {
+        // 如果没有 phrase 上没有 key 属性 则用序号代替 variable key
+        const variableKey = (phrase as any).key || `variable_${(variableIndex += 1)}`;
+        variableMap[variableKey] = {
+          ...phrase,
+        };
+        return {
+          type: 'variable',
+          children: [
+            {
+              text: '',
+            },
+          ],
+          value: phrase.value,
+          metadata: phrase.metadata,
+          key: variableKey,
+        };
+      }
+      case 'custom':
+        // TODO CustomPhraseSpec 时的转换: 内置的 custom 类型，可以提供内置定制转换，外部的需要自己提供转换函数，否则使用 phrase.value 兜底
+        return {
+          type: phrase.type,
+          text: phrase.value,
+          metadata: phrase.metadata,
+        };
+      default:
+        return { text: '' };
+    }
+  };
+
+  const convertPhraseArray = (phrases: PhraseSpec[]) => {
+    const elements: TDescendant[] = [];
+    phrases.forEach((phrase) => {
+      if (phrase.type === 'text') {
+        elements.push(convertPhrase(phrase));
+      } else {
+        elements.push({ text: ' ' });
+        elements.push(convertPhrase(phrase));
+        elements.push({ text: ' ' });
+      }
+    });
+    return elements;
+  };
+
+  // 块级元素转换
+  // TODO 应该在 text-schema 中，每个 spec 自己提供转换函数吗
+  const convertParagraph = (paragraph: ParagraphSpec) => {
+    switch (paragraph?.type) {
+      case 'normal':
+        return {
+          type: 'p',
+          children: convertPhraseArray((paragraph as TextParagraphSpec).phrases),
+        };
+      case 'bullets': {
+        const bullets = (paragraph as BulletsParagraphSpec).bullets.map((bullet) => {
+          const { phrases, subBullet } = bullet;
+          return {
+            type: 'li',
+            children: convertPhraseArray(phrases).concat(convertParagraph(subBullet) || []),
+          };
+        });
+        return {
+          type: 'ul',
+          children: bullets,
+        };
+      }
+      case 'heading1':
+      case 'heading2':
+      case 'heading3':
+      case 'heading4':
+      case 'heading5':
+      case 'heading6':
+        return {
+          type: `h${paragraph.type[paragraph.type.length - 1]}`,
+          children: convertPhraseArray((paragraph as HeadingParagraphSpec).phrases),
+        };
+      default:
+        // TODO 当 paragraph 为 CustomMetaData 类型时
+        return { text: '' };
+    }
+  };
+  const { headline, sections } = spec;
+  if (headline?.phrases) {
+    content.push({
+      type: 'h1',
+      children: convertPhraseArray(headline.phrases),
+    });
+  }
+  sections?.forEach((section) => {
+    if (section.paragraphs) {
+      (section.paragraphs as ParagraphSpec[])?.forEach((paragraph) => {
+        content.push(convertParagraph(paragraph));
+      });
+    }
+  });
+
+  return {
+    content: content.length > 0 ? content : undefined,
+    variableMap,
+  };
+};
---
+++ packages/NarrativeTextEditor/src/helpers/toTextSchema.ts
@@ -0,0 +1,110 @@
+import type { HeadlineSpec, NarrativeTextSpec, ParagraphSpec, PhraseSpec } from '@antv/narrative-text-vis';
+import type { TDescendant } from '@udecode/plate';
+
+// 将 NarrativeVisEditor 的 content value 转换为 text-schema
+export const editorPropsToSpec = (elements: TDescendant[]) => {
+  // 将内置元素转换成 spec
+  const convertElement = (element: TDescendant) => {
+    const getChildren = (children: TDescendant[]): PhraseSpec[] => {
+      return children?.map((child) => convertElement(child) as PhraseSpec).filter((phrase: PhraseSpec) => phrase);
+    };
+
+    switch (element?.type) {
+      case 'variable':
+        return {
+          type: 'entity',
+          value: element.value,
+          key: element.key,
+          metadata: element.metadata,
+        };
+      case 'h1':
+        return {
+          type: 'headline',
+          phrases: getChildren(element.children),
+        };
+      case 'p':
+        return {
+          type: 'normal',
+          phrases: getChildren(element.children),
+        };
+      case 'ul':
+        return {
+          type: 'bullets',
+          isOrder: false,
+          bullets: getChildren(element.children),
+        };
+      case 'ol':
+        return {
+          type: 'bullets',
+          isOrder: true,
+          bullets: getChildren(element.children),
+        };
+      case 'li': {
+        const phraseChildren: TDescendant[] = [];
+        let subBulletChild;
+        element.children?.forEach((child: TDescendant) => {
+          // 编辑后会变成 children 是 lic 类型的, 特殊处理
+          if (child.type === 'lic' && child.children?.length > 0) {
+            phraseChildren.push(...child.children);
+          } else if (child.type === 'ul' || child.type === 'ol') {
+            subBulletChild = child;
+          } else {
+            phraseChildren.push(child);
+          }
+        });
+        return {
+          type: 'bullet-item',
+          phrases: getChildren(phraseChildren),
+          subBullet: subBulletChild && convertElement(subBulletChild),
+        };
+      }
+      default: {
+        // TODO 支持 custom 元素
+        if (element.text) {
+          return {
+            type: 'text',
+            value: element.text || element.value,
+          };
+        }
+        return {
+          type: 'text',
+          value: '',
+        };
+      }
+    }
+  };
+
+  const paragraphs: ParagraphSpec[] = [];
+  const spec: NarrativeTextSpec = {
+    // TODO editor 实际上没有 headline 和 sections 的划分，也没有多个 section
+    headline: undefined,
+    sections: [
+      {
+        paragraphs,
+      },
+    ],
+  };
+  for (let i = 0; i < elements.length; i += 1) {
+    const element = elements[i];
+    // 处理块级元素外层结构
+    switch (element.type) {
+      case 'h1': {
+        const headlineSpec = convertElement(element);
+        if (!spec.headline && headlineSpec) {
+          spec.headline = headlineSpec as HeadlineSpec;
+        } else {
+          paragraphs.push(headlineSpec as ParagraphSpec);
+        }
+        break;
+      }
+      case 'p':
+      case 'ul':
+      case 'ol':
+        paragraphs.push(convertElement(element) as ParagraphSpec);
+        break;
+      default:
+        break;
+    }
+  }
+  return spec;
+};
