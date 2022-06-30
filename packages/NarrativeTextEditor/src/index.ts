ccd90b1d5f05886768e02574f7ab0615bf281375
xi li <35586469+BBSQQ@users.noreply.github.com>
2022-06-30 21:36:25 +0800

    feat(editor): ✨ enable to change editor value (#55)

--- packages/NarrativeTextEditor/src/index.ts
+++ packages/NarrativeTextEditor/src/index.ts
@@ -1,5 +1,5 @@
-import { TemplateEditor } from './TemplateEditor';
-import { NarrativeTextEditor as InnerNarrativeTextEditor } from './editor';
+import TemplateEditor from './TemplateEditor';
+import InnerNarrativeTextEditor from './editor';
 
 type NarrativeTextEditor = typeof InnerNarrativeTextEditor & {
   Template: typeof TemplateEditor;
@@ -11,5 +11,5 @@ NarrativeTextEditor.Template = TemplateEditor;
 export { ELEMENT_VARIABLE, ELEMENT_VARIABLE_INPUT } from './plugins/variable';
 export { CustomBlockToolbarButton, CustomInlineToolbarButton } from './plugins/custom';
 
-export type { NarrativeTextEditorProps, TemplateEditorProps } from './types';
+export type { NarrativeTextEditorProps, NarrativeTextEditorRef, TemplateEditorProps } from './types';
 export type { CustomElementComponent } from './plugins/custom';
