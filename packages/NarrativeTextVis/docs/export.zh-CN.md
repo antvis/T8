587f6e11e1fcfa970e5b73ee4a236cd5d57e4bb2
Ray Bomb <73281388+leipeng1223@users.noreply.github.com>
2022-07-26 11:03:05 +0800

    feat: export markdown (#59)
    * feat: support basic markdown output
    
    * fix: 🐛 changed demo and removed single unuseful line
    
    * fix: 🐛 changed import method
    
    * fix: 🐛 removed annotations
    
    * refactor: ♻️ renamed functions in MarkdownExporter, added explanations and new mock data
    
    * refactor: ♻️ merged MarkdownExporter into TextExporter
    
    * refactor: ♻️ add function getMarkdown into PhraseDescriptor and BlockDescriptor
    
    * fix: 🐛 remove unuseful lines and add annotations and JSDoc

--- packages/NarrativeTextVis/docs/export.zh-CN.md
+++ packages/NarrativeTextVis/docs/export.zh-CN.md
@@ -26,7 +26,8 @@ import booking from './mock/booking.json';
 const exporter = new TextExporter();
 
 function getSignAssessmentText(value, metadata) {
-  return `${metadata?.assessment === 'negative' ? '-' : metadata?.assessment === 'positive'? '+': ''}${value}`;
+  if (metadata?.assessment === 'negative') return `-${value}`
+  return `${metadata?.assessment === 'positive'? '+': ''}${value}`;
 }
 
 const exportWithSign = new TextExporter([
@@ -60,8 +61,55 @@ export default () => {
 }
 ```
 
-## 导出 md
-施工中...
+## 导出 Markdown
+
+在`TextExporter`中调用`getNarrativeMarkdown()`即可导出Markdown文本。类似的，可通过定制 plugin descriptor 中的 getMarkdown 定义复制行为。
+
+```jsx
+import React from 'react';
+import { Space, Button, message } from 'antd';
+import { CopyOutlined } from '@ant-design/icons';
+import copy from 'copy-to-clipboard';
+import { NarrativeTextVis, TextExporter, createRatioValue, createDeltaValue } from '@antv/narrative-text-vis';
+import sample from './mock/markdownSample.json';
+
+function getSignAssessmentText(value, metadata) {
+  if (metadata?.assessment === 'negative') return `-${value}`
+  return `${metadata?.assessment === 'positive'? '+': ''}${value}`;
+}
+
+const exportMarkdown = new TextExporter();
+
+const exportMarkdownWithSign = new TextExporter([
+  createRatioValue({ getMarkdown: getSignAssessmentText }),
+  createDeltaValue({ getMarkdown: getSignAssessmentText }),
+]);
+
+export default () => {
+  return (
+    <>
+      <Space>
+        <Button 
+          type="primary"
+          icon={<CopyOutlined/>} 
+          onClick={() => {
+            const res = copy(exportMarkdown.getNarrativeMarkdown(sample));
+            if (res) message.success('复制成功');
+          }}
+        >复制默认Markdown</Button>
+        <Button 
+          icon={<CopyOutlined/>} 
+          onClick={() => {
+            const res = copy(exportMarkdownWithSign.getNarrativeMarkdown(sample));
+            if (res) message.success('复制成功');
+          }}
+        >复制带正号Markdown</Button>
+      </Space>
+      <NarrativeTextVis spec={sample} />
+    </>
+  )
+}
+```
 
 ## 导出富文本
 施工中...
--- packages/NarrativeTextVis/src/chore/exporter/TextExporter.ts
+++ packages/NarrativeTextVis/src/chore/exporter/TextExporter.ts
@@ -12,11 +12,17 @@ import {
   isCustomParagraph,
   isCustomPhrase,
   isEntityPhrase,
+  getHeadingWeight,
 } from '@antv/narrative-text-schema';
 import { pad } from 'lodash';
 import { PluginManager } from '../plugin';
 
 export class TextExporter extends PluginManager {
+  /**
+   * Export your text schema as pure text.
+   * @param {NarrativeTextSpec} spec - the narrative text schema you want to export
+   * @returns
+   */
   getNarrativeText(spec: NarrativeTextSpec) {
     let text = '';
     if (spec?.headline?.phrases) text += this.getPhrasesText(spec.headline.phrases);
@@ -25,6 +31,7 @@ export class TextExporter extends PluginManager {
     }
     return text;
   }
+
   getSectionText(spec: SectionSpec) {
     if (isStandardSection(spec)) {
       return spec.paragraphs.reduce((prev, curr) => `${prev}\r\n${this.getParagraphText(curr)}`, '');
@@ -37,12 +44,16 @@ export class TextExporter extends PluginManager {
     }
     return '';
   }
+
   getParagraphText(spec: ParagraphSpec) {
     if (isHeadingParagraph(spec) || isTextParagraph(spec)) return this.getPhrasesText(spec.phrases);
     if (isBulletParagraph(spec)) {
+      const level = 1;
       return spec.bullets?.reduce(
         (prev, curr, index) =>
-          `${prev}${prev ? '\r\n' : ''}${spec.isOrder ? `${index + 1}` : '· '}${this.getBulletsText(curr, 1)}`,
+          // if is ordered bullet, add '1. xxxxxx'
+          // else, add '· xxxxxx'
+          `${prev}${prev ? '\r\n' : ''}${spec.isOrder ? `${index + 1}` : '· '}${this.getBulletsText(curr, level)}`,
         '',
       );
     }
@@ -54,6 +65,7 @@ export class TextExporter extends PluginManager {
     }
     return '';
   }
+
   getBulletsText(spec: BulletItemSpec, level = 1): string {
     let text = '';
     if (spec?.phrases) {
@@ -62,6 +74,9 @@ export class TextExporter extends PluginManager {
     if (spec?.subBullet) {
       text = spec.subBullet.bullets?.reduce(
         (prev, curr, index) =>
+          // if is ordered bullet, add paddings + '1. xxxxxx'
+          // else, add paddings + '· xxxxxx'
+          // padding number here is 2
           `${prev}\r\n${pad('', level * 2)}${spec.subBullet.isOrder ? `${index + 1}` : '· '}${this.getBulletsText(
             curr,
             level + 1,
@@ -71,6 +86,7 @@ export class TextExporter extends PluginManager {
     }
     return text;
   }
+
   getPhrasesText(spec: PhraseSpec[]) {
     return spec.reduce((prev, curr) => {
       let text = curr?.value;
@@ -78,6 +94,115 @@ export class TextExporter extends PluginManager {
         const descriptor = this.getPhraseDescriptorBySpec(curr);
         if (descriptor && descriptor?.getText) {
           text = descriptor.getText(curr.value, curr.metadata);
+        }
+      }
+      return prev + text;
+    }, '');
+  }
+
+  /**
+   * Export your text schema as Markdown.
+   * @param {NarrativeTextSpec} spec - the narrative text schema you want to export
+   * @returns
+   */
+  getNarrativeMarkdown(spec: NarrativeTextSpec) {
+    let text = '';
+    // if there is a headline, add '# Headline' and a horizontal rule beneath
+    if (spec?.headline?.phrases) text += `# ${this.getPhrasesMarkdown(spec.headline.phrases)}\r\n\r\n---`;
+    if (spec?.sections) {
+      text = spec?.sections?.reduce(
+        (prev, curr) => `${prev}${prev ? '\r\n' : ''}${this.getSectionMarkdown(curr)}`,
+        text,
+      );
+    }
+    return text;
+  }
+
+  getSectionMarkdown(spec: SectionSpec) {
+    if (isStandardSection(spec)) {
+      return spec.paragraphs.reduce((prev, curr) => `${prev}\r\n${this.getParagraphMarkdown(curr)}`, '');
+    }
+    if (isCustomSection(spec)) {
+      const descriptor = this.getBlockDescriptor(spec.customType);
+      if (descriptor?.getMarkdown) {
+        return descriptor.getMarkdown(spec);
+      }
+      if (descriptor?.getText) {
+        return descriptor.getText(spec);
+      }
+    }
+    return '';
+  }
+
+  getParagraphMarkdown(spec: ParagraphSpec) {
+    if (isTextParagraph(spec)) return this.getPhrasesMarkdown(spec.phrases);
+    if (isHeadingParagraph(spec)) {
+      return `${'#'.repeat(getHeadingWeight(spec.type))} ${this.getPhrasesMarkdown(spec.phrases)}`;
+    }
+    if (isBulletParagraph(spec)) {
+      const level = 1;
+      return spec.bullets?.reduce(
+        (prev, curr, index) =>
+          // if is ordered bullet, add '1. xxxxxx'
+          // else, add '- xxxxxx'
+          `${prev}${prev ? '\r\n' : ''}${spec.isOrder ? `${index + 1}. ` : '- '}${this.getBulletsMarkdown(
+            curr,
+            level,
+          )}`,
+        '',
+      );
+    }
+    if (isCustomParagraph(spec)) {
+      const descriptor = this.getBlockDescriptor(spec.customType);
+      if (descriptor?.getMarkdown) {
+        return descriptor.getMarkdown(spec);
+      }
+      if (descriptor?.getText) {
+        return descriptor.getText(spec);
+      }
+    }
+    return '';
+  }
+
+  getBulletsMarkdown(spec: BulletItemSpec, level = 1): string {
+    let text = '';
+    if (spec?.phrases) {
+      text = this.getPhrasesMarkdown(spec.phrases);
+    }
+    if (spec?.subBullet) {
+      text = spec.subBullet.bullets?.reduce(
+        (prev, curr, index) =>
+          // if is ordered bullet, add paddings + '1. xxxxxx'
+          // else, add paddings + '- xxxxxx'
+          // padding number in markdown should be 4
+          `${prev}\r\n${pad('', level * 4)}${spec.subBullet.isOrder ? `${index + 1}. ` : '- '}${this.getBulletsMarkdown(
+            curr,
+            level + 1,
+          )}`,
+        text,
+      );
+    }
+    return text;
+  }
+
+  // TODO: in getPhrasesMarkdown, support styles defined by entity type and user customization
+  getPhrasesMarkdown(spec: PhraseSpec[]) {
+    return spec.reduce((prev, curr) => {
+      let text = curr?.value;
+      if (isEntityPhrase(curr) || isCustomPhrase(curr)) {
+        const descriptor = this.getPhraseDescriptorBySpec(curr);
+        if (descriptor?.getMarkdown) {
+          return descriptor.getMarkdown(curr.value, curr.metadata);
+        }
+        if (descriptor?.getText) {
+          return descriptor.getText(curr.value, curr.metadata);
+        }
+      } else {
+        if (curr.bold) {
+          text = `**${text}**`;
+        }
+        if (curr.italic) {
+          text = `*${text}*`;
         }
       }
       return prev + text;
